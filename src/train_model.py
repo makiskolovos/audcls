07179b0c380873c2fcfc1f9148fdad567dba0ecc
makis <gkolovos1@gmail.com>
2023-05-26 04:54:52 +0000

    minor changes to train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -7,7 +7,7 @@ import pandas as pd
 from model import CustomHyperModel
 from evaluate_model import Evaluator
 from data_loader import DataLoader
-from conf import DIRECTORY, OBJECTIVE, PROJECT_NAME, MODEL_DIR, HISTORY, SAMPLE_TYPE, EPOCHS, BATCH_SIZE
+from conf import DIRECTORY, OBJECTIVE, PROJECT_NAME, MODEL_DIR, HISTORY, SAMPLE_TYPE, EPOCHS, BATCH_SIZE, MAX_TRIALS
 
 
 def hp_optimization():
@@ -15,30 +15,29 @@ def hp_optimization():
     The hp_optimization function is a wrapper for the KerasTuner API. It takes in no arguments, and returns a compiled
     model with optimized hyperparameters. The function uses Bayesian Optimization to search through the space of
     hyperparameters defined by CustomHyperModel(). The objective function used to evaluate each model is defined by
-    OBJECTIVE(), which calls train_and_evaluate() on each model.
+    OBJECTIVE, which calls train_and_evaluate() on each model.
 
     :return: The best model
     :doc-author: Trelent
     """
     tuner = kt.BayesianOptimization(objective=OBJECTIVE, directory=DIRECTORY, project_name=PROJECT_NAME,
-                                    hypermodel=CustomHyperModel(), max_trials=1, overwrite=False, seed=None)
+                                    hypermodel=CustomHyperModel(), max_trials=MAX_TRIALS, overwrite=False, seed=None)
 
     tuner.search()
     best_hyperparameters = tuner.get_best_hyperparameters()[0]
     [print(f"{name}: {best_hyperparameters.get(name)}") for name in best_hyperparameters.values]
 
     best_model = tuner.hypermodel.build(best_hyperparameters)
+
     return best_model
 
 
-def train_best_model(best_model):
+def train_model(model):
     """
-    The train_best_model function trains the best model found in the hyperparameter search.
-    It saves the trained model to disk and writes a history of training metrics to a csv file.
-    Finally, it evaluates performance on both samples and songs.
+    The train_model function trains the model.
 
-    :param best_model: Pass the model to be trained
-    :return: A history object
+    :param model: Specify the model to be trained
+    :return: The trained model
     :doc-author: Trelent
     """
     if not os.path.exists(MODEL_DIR):
@@ -47,25 +46,32 @@ def train_best_model(best_model):
                                                  patience=10,
                                                  restore_best_weights=True)
 
-        history = best_model.fit(x=DataLoader(mode='train',
-                                              sample_type=SAMPLE_TYPE).load_ds(batch_size=BATCH_SIZE),
-                                 validation_data=DataLoader(mode='eval',
-                                                            sample_type=SAMPLE_TYPE).load_ds(batch_size=BATCH_SIZE),
-                                 callbacks=[es_cb],
-                                 epochs=EPOCHS,
-                                 verbose=1)
+        model.fit(x=DataLoader(mode='train', sample_type=SAMPLE_TYPE).load_ds(batch_size=BATCH_SIZE),
+                  validation_data=DataLoader(mode='eval', sample_type=SAMPLE_TYPE).load_ds(batch_size=BATCH_SIZE),
+                  callbacks=[es_cb],
+                  epochs=EPOCHS,
+                  verbose=1)
 
-        best_model.save(MODEL_DIR)
-        pd.DataFrame(history.history).to_csv(HISTORY)
+        pd.DataFrame(model.history.history).to_csv(HISTORY, index=False)
+        model.save(MODEL_DIR)
+        return model
 
-    # Run after saving model
-    evaluator = Evaluator()
+
+def evaluate_model(trained_model):
+    """
+    The evaluate_model function takes a trained model and evaluates it on the test set.
+    It saves metrics per sample, metrics per song, and writes results to a csv file.
+
+    :param trained_model: Pass the trained model to the evaluator class
+    :return: None
+    :doc-author: Trelent
+    """
+    evaluator = Evaluator(trained_model)
     evaluator.metrics_per_sample()
     evaluator.metrics_per_song()
     evaluator.results_to_csv()
 
 
 if __name__ == '__main__':
-    train_best_model(hp_optimization())
-
-exit()
+    evaluate_model(train_model(hp_optimization()))
+    exit()
